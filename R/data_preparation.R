17d6da3777affb6b99247518cca250cb0c5efeeb
Barbara Vreede <b.vreede@gmail.com>
2020-07-07 14:04:56 +0200

    add penguin data

---
+++ R/data_preparation.R
@@ -0,0 +1,22 @@
+# Data preparation for the WORCShop
+
+# The following is only necessary for installation of palmerpenguins
+# I'm leaving it in because installation is from github, not CRAN
+# remotes::install_github("allisonhorst/palmerpenguins") 
+
+library(haven)
+library(readr)
+library(writexl)
+library(palmerpenguins)
+
+penguins <- palmerpenguins::penguins
+
+# create directory
+if(!dir.exists("data/")){
+  dir.create("data/")
+}
+
+# save the data as spss, csv, and xlsx
+write_sav(penguins, "data/penguins.sav")
+write_csv(penguins, "data/penguins.csv")
+write_xlsx(penguins, "data/penguins.xlsx")
